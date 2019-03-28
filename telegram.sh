12e52b03d021c6a8246f9c3baeb114071fdfccbd
Mohd Faraz <androiabledroid@gmail.com>
2019-03-28 12:32:00 +0000

    PBRP: Fixed Scripting
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- telegram.sh
+++ telegram.sh
@@ -2,14 +2,15 @@
 
 if [ -n $BOT_API ];
 then
+ZIP_NAME=$(basename "$ZIP_NAME")
 URL="https://sourceforge.net/projects/pitchblack-twrp/files/"$NAME"/"$ZIP_NAME;
 
-./telegram -t ${BOT_API} -c @pbtwrp -M "PitchBlack Recovery for LYF Water 8 (mobee01a)"$'\n'"Info"$'\n'"PitckBlack v2.9.0 Official"$'\n'"Based on TWRP 3.2.3"
+$(pwd)/telegram -t ${BOT_API} -c @pbtwrp -M "PitchBlack Recovery for LYF Water 8 (mobee01a)"$'\n'"Info"$'\n\n'"PitckBlack v2.9.0 Official"$'\n'"Based on TWRP 3.2.3"
 
 MD5=`md5sum ${OUT}/$ZIP_NAME | awk '{ print $1 }'`
-./telegram -t ${BOT_API} -c @pbtwrp -C "MD5: $MD5"$'\n\n'
+$(pwd)/telegram -t ${BOT_API} -c @pbtwrp -C "MD5: $MD5"$'\n\n'
 
-./telegram -t ${BOT_API} -c @pbtwrp -M "Download: [Link](${URL})"$'\n\n'"Channel: @pbtwrp"$'\n'"Chat:@pbrpcom"
+$(pwd)/telegram -t ${BOT_API} -c @pbtwrp -M "Download: [Link](${URL})"$'\n\n'"Channel: @pbtwrp"$'\n'"Chat:@pbrpcom"
 
 unset NAME;
 unset BOT_API;
