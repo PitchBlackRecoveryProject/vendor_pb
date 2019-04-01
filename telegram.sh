c953005a4ced2940d9fe6b44cd43f7d5dbc318ba
Mohd Faraz <androiabledroid@gmail.com>
2019-04-01 21:38:25 +0000

    PBRP: Derps
     * Condition to be asked whether user want to upload or not.
    
     * Tele msg update"
    
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- telegram.sh
+++ telegram.sh
@@ -2,15 +2,14 @@
 
 if [ -n $BOT_API ];
 then
+DEVICE=$(cut -d'_' -f2-3 <<<$TARGET_PRODUCT)
+MODEL=$TARGET_DEVICE
+VN=$TARGET_VENDOR
 ZIP_NAME=$(basename "$ZIP_NAME")
 URL="https://sourceforge.net/projects/pitchblack-twrp/files/"$NAME"/"$ZIP_NAME;
-
-$(pwd)/telegram -t ${BOT_API} -c @pbtwrp -M "PitchBlack Recovery for LYF Water 8 (mobee01a)"$'\n'"Info"$'\n\n'"PitckBlack v2.9.0 Official"$'\n'"Based on TWRP 3.2.3"
-
 MD5=`md5sum ${OUT}/$ZIP_NAME | awk '{ print $1 }'`
-$(pwd)/telegram -t ${BOT_API} -c @pbtwrp -C "MD5: $MD5"$'\n\n'
 
-$(pwd)/telegram -t ${BOT_API} -c @pbtwrp -M "Download: [Link](${URL})"$'\n\n'"Channel: @pbtwrp"$'\n'"Chat:@pbrpcom"
+$(pwd)/telegram -t ${BOT_API} -c @pbtwrp -M "PitchBlack Recovery for $VN $MODEL ($DEVICE)"$'\n'"Info"$'\n\n'"PitckBlack v2.9.0 Official"$'\n'"Based on TWRP 3.2.3"$'\n'"MD5: \`$MD5\`"$'\n\n'"Download: [Link](${URL})"$'\n\n'"Channel: @pbtwrp"$'\n'"Chat:@pbrpcom"
 
 unset NAME;
 unset BOT_API;
