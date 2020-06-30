omnirom/android_vendor_omni

vendor: add omni Boardconfig
Change-Id: Ib8681a8475372da8c8edbe38604a936359fbec1f

config/BoardConfigOmni.mk
@@ -1,0 +1,3 @@
+include vendor/omni/config/BoardConfigKernel.mk
+include vendor/omni/config/BoardConfigSoong.mk
+

PitchBlackRecoveryProject/vendor_pb

config/BoardConfigOmni.mk
@@ -1,0 +1,2 @@
+include vendor/pb/config/BoardConfigKernel.mk
+include vendor/pb/config/BoardConfigSoong.mk