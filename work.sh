928b9a6761090d429c90b00c3165131ff77c658a
Mohd Faraz <androiabledroid@gmail.com>
2020-07-24 15:26:03 +0530

    PBRP: Switch to new
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- work.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm _data/devices.json
-curl https://raw.githubusercontent.com/PitchBlackRecoveryProject/vendor_pb/pb/pb_devices.json > _data/devices.json
-git add _data/devices.json
-git -c user.name="PitchBlack-BOT" -c user.email="pitchblackrecovery@gmail.com" commit -m "Added New devices"
-
-exit 0
