d678823902b8ddb1fb9acaf3c6ff6215210cfc19
nebrassy <nebras30@gmail.com>
2021-05-02 17:14:51 +0200

    pb: add twrp cmdline for recovery as boot devices
    Change-Id: Ib8c6f4f824463dffbbfd7505b369a97a6b6d2db0
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- config/BoardConfigOmni.mk
+++ config/BoardConfigOmni.mk
@@ -1,2 +1,6 @@
 include vendor/pb/config/BoardConfigKernel.mk
 include vendor/pb/config/BoardConfigSoong.mk
+
+ifeq ($(BOARD_USES_RECOVERY_AS_BOOT),true)
+GENERIC_KERNEL_CMDLINE += twrpfastboot=1
+endif
