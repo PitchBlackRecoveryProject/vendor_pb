7a019852e6860638db35c448e676fa0a14e4d9eb
nebrassy <nebras30@gmail.com>
2021-09-12 17:29:19 +0200

    also set twrp cmdline for legacy A/B
    Change-Id: Ibb3d7076d6bd1518585c4f3b3bfcc71cc8e0ceae
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- config/BoardConfigPb.mk
+++ config/BoardConfigPb.mk
@@ -2,5 +2,9 @@ include vendor/pb/config/BoardConfigKernel.mk
 include vendor/pb/config/BoardConfigSoong.mk
 
 ifeq ($(BOARD_USES_RECOVERY_AS_BOOT),true)
-GENERIC_KERNEL_CMDLINE += twrpfastboot=1
+    ifeq ($(BOARD_BOOT_HEADER_VERSION),3)
+         GENERIC_KERNEL_CMDLINE += twrpfastboot=1
+    else
+        INTERNAL_KERNEL_CMDLINE += twrpfastboot=1
+    endif
 endif
