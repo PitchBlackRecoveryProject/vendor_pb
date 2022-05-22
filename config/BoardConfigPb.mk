62f927de38b243d7b1e7f1d8d1c37516ccfa0e90
nebrassy <nebras30@gmail.com>
2022-05-22 13:36:06 +0200

    add twrpfastboot=1 to GENERIC_KERNEL_CMDLINE for all vendor_boot devices
    Change-Id: Ic9844cb1f1cf0869d06b96a64b6e9dc713ba34e5

--- config/BoardConfigPb.mk
+++ config/BoardConfigPb.mk
@@ -2,7 +2,7 @@ include vendor/pb/config/BoardConfigKernel.mk
 include vendor/pb/config/BoardConfigSoong.mk
 
 ifeq ($(BOARD_USES_RECOVERY_AS_BOOT),true)
-    ifeq (true,$(BOARD_USES_GENERIC_KERNEL_IMAGE))
+    ifeq (true,$(BUILDING_VENDOR_BOOT_IMAGE))
         GENERIC_KERNEL_CMDLINE += twrpfastboot=1
     else
         INTERNAL_KERNEL_CMDLINE += twrpfastboot=1
