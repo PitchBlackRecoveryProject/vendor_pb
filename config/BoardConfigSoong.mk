670af89255583c68d2eddeca92d509e9407f34a0
Mohd Faraz <androiabledroid@gmail.com>
2022-09-28 08:19:55 +0200

    pb: soong: fix some derps
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -26,10 +26,12 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 SOONG_CONFIG_NAMESPACES += pbrpGlobalVars
 SOONG_CONFIG_pbrpGlobalVars += \
     target_enforce_ab_ota_partition_list \
-    target_init_vendor_lib
+    target_init_vendor_lib \
+    legacy_hw_disk_encryption
 
 # Soong bool variables
 SOONG_CONFIG_pbrpGlobalVars_target_enforce_ab_ota_partition_list := $(TARGET_ENFORCE_AB_OTA_PARTITION_LIST)
+SOONG_CONFIG_pbrpGlobalVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK_ENCRYPTION)
 
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
