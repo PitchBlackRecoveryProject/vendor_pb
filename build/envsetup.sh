96be54efe38b5ca1b7d84221b35c2d3d6f4ff9c1
captainthrowback <captainthrowback@hotmail.com>
2023-11-01 08:23:55 +0700

    envsetup: export TOP
    In build systems where OUT_DIR_COMMON_BASE is used,
    TOP cannot be identified if the directory is changed.
    Exporting TOP when envsetup is run ensures that it
    will always be set and croot will work as expected.
    
    Change-Id: I6944ac7d3513ba9504e03baa5f9910bddb126d7c

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -150,6 +150,9 @@ function cafremote()
     echo "Remote 'caf' created"
 }
 
+# Set $TOP
+export TOP=$(gettop)
+
 # Enable SD-LLVM if available
 if [ -d $(gettop)/vendor/qcom/sdclang ]; then
             export SDCLANG=true
