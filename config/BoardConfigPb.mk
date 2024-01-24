f68071f3e054ebdcdb8a87e8b7cd8ca1232a9260
sushrut1101 <guptasushrut@gmail.com>
2024-01-24 09:54:02 +0700

    Add TW_NO_FASTBOOT_BOOT flag
    * Some Devices that do not support hotbooting an image using fastboot
      boot (eg: mtk devices) face issues like battery level detection,
      unable to reboot to system, etc.
      When twrpfastboot is set to 1 in the kernel cmdline. Setting this flag
      to true will not add twrpfastboot=1 in the cmdline and therefore will
      fix those issues.
    
    * Tested on evergo, fixed battery level detection, reboot to system and
      slot changing.
    
    Change-Id: I3f2d0fb3ef71f42904f756b4c7dec1cc51905f40
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- config/BoardConfigPb.mk
+++ config/BoardConfigPb.mk
@@ -2,9 +2,11 @@ include vendor/pb/config/BoardConfigKernel.mk
 include vendor/pb/config/BoardConfigSoong.mk
 
 ifeq ($(BOARD_USES_RECOVERY_AS_BOOT),true)
-    ifeq (true,$(BUILDING_VENDOR_BOOT_IMAGE))
-        GENERIC_KERNEL_CMDLINE += twrpfastboot=1
-    else
-        INTERNAL_KERNEL_CMDLINE += twrpfastboot=1
+    ifneq ($(TW_NO_FASTBOOT_BOOT),true)
+        ifeq (true,$(BUILDING_VENDOR_BOOT_IMAGE))
+            GENERIC_KERNEL_CMDLINE += twrpfastboot=1
+        else
+            INTERNAL_KERNEL_CMDLINE += twrpfastboot=1
+        endif
     endif
 endif
